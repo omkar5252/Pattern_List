e4829175224e169ac9768be4ce76be994f513707
Omkar Dorugade <100912340+omkar5252@users.noreply.github.com>
2022-05-02 16:58:03 +0530

    Update and rename 1.py to Write a program to find sum of all elements of list.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-# Problem 1 : Write a program to find sum of all elements of list
-
-data = [1,2,3,4,5]
-sum = 0
-for x in data:
-    sum = sum + x
-print("Sum of all element of list:",sum)
---
+++ Write a program to find sum of all elements of list.py
@@ -0,0 +1,7 @@
+# Write a program to find sum of all elements of list
+
+data = [1,2,3,4,5]
+sum = 0
+for x in data:
+    sum = sum + x
+print("Sum of all element of list:",sum)
