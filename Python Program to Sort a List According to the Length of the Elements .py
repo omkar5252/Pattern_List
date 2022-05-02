3c2b60efacd3e7e9b36de903a215ad873c78763d
Omkar Dorugade <100912340+omkar5252@users.noreply.github.com>
2022-05-02 16:55:28 +0530

    Update and rename 5.py to Python Program to Sort a List According to the Length of the Elements .py

--- Python Program to Sort a List According to the Length of the Elements .py
+++ Python Program to Sort a List According to the Length of the Elements .py
@@ -1,4 +1,4 @@
-# Problem no 5 : Python Program to Sort a List According to the Length of the Elements 
+# Python Program to Sort a List According to the Length of the Elements 
 # within the list.
 
 list = ["bicycle","truck","car"]
